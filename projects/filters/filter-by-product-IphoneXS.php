202746d5275da36c0f9d4200a61b8d762b3a4ffd
KCTHolman <46706715+KCTHolman@users.noreply.github.com>
2019-01-29 14:51:34 +0100

    filteren op product gelukt

---
+++ projects/filters/filter-by-product-IphoneXS.php
@@ -0,0 +1,22 @@
+<?php
+
+    //CARD.PHP
+
+    include "../../php-includes/db-connection.php";        
+
+    $sql_querie = "SELECT product_price, product_name, product_image_1 FROM products  WHERE product_type_mobile = 'Iphone XS'
+    ";
+    
+    $db_result = $conn->query($sql_querie);  
+
+    foreach ($db_result as $row)
+    {            
+        echo    '<div class="cards">
+        <div class="card-image"><img src="../../images/products/' . $row['product_image_1'] . '"></div>
+        <div class="card-name">' . $row['product_name'] . '</div>
+        <div class="card-price">'. $row ['product_price'] . '</div>
+    </div>';
+    }
+    $conn = null;
+
+?>
