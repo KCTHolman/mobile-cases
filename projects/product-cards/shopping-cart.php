4010656e5590625e078356bcd2681f40465e6f6e
KCTHolman <46706715+KCTHolman@users.noreply.github.com>
2019-02-07 09:58:30 +0100

    aanpassing bestanden shopping cart

--- projects/product-cards/shopping-cart.php
+++ projects/product-cards/shopping-cart.php
@@ -1,84 +1,27 @@
 <?php   session_start();
  include "../../php-includes/db-connection.php";  
  
- 
-
  $productID = $_GET['product_id'];
  $productPrice = $_GET['product_price'];
  $productAmount = $_GET['amount_cart'];
 
-
- /////////////////  hiermee zet je de strings in een sessie/////////////////////
-/*  
- if(isset($_SESSION['product_id'])){
-    var_dump( $_SESSION['product_id'] );
- 
- }
- if(isset($_SESSION['product_price'])){
-    var_dump( $_SESSION['product_price'] );
- }
- 
-if(isset($_SESSION['amount_cart'])){
-   var_dump( $_SESSION['amount_cart'] );
-}
- */
- ///////////////   HIERMEE ZET IK DE SESSIE IN EEN ARRAY  ///////////////////////////////////
-
-
-
-//  if(isset($_SESSION['shopping-cart'])){
-//     var_dump( $_SESSION['shopping-cart'] );
-//  }
-
- /////////////////////////////
-
-
- /// ingevuld om een testomgeving te hebben 
-
-
  $shoppingCart = array(
-/*     array("id"=>"1","prize"=>"24", "amount" =>1 ),
-    array("id"=>"2","prize"=>"25", "amount" =>1 ),
-    array("id"=>"3","prize"=>"14","amount" =>1 ),
-    array("id"=>"4","prize"=>"3", "amount" =>1 ), */
-    
-
- ); //Array voor shoppingCart items;
+ ); 
 
  $newShoppingCartItem = array("id" => $productID, "amount" => $productAmount, "prize" => $productPrice);
 
- //array_push($shoppingCart, $newShoppingCartItem);  // hiermee push ik een nieuw item in mijn session array 
-
- 
- //var_dump($shoppingCart); 
-
  $_SESSION['shopping-cart'] = $shoppingCart; 
 
-/*  foreach ($shoppingCart as $shoppingItem){
-     echo $shoppingItem['id'];
-     echo "<br>";
- } */
-
-
-
-/*  echo searchForId(2, $shoppingCart);
-
- echo searchForId(50, $shoppingCart);
-
- echo searchForId(4, $shoppingCart);
- */              /// hij zal dan 2, null 3 als uitkomst geven
- /////////////////////
-
- function searchForId($ID, $ARRAY) {       // $ID als ik 3 invul komt als uitkomst 2 (0,1,2) 
-    foreach ($ARRAY as $key => $val) {     // $val is het stukje wat je uit je array plukt   $key is de [0] of [1] array 
+ function searchForId($ID, $ARRAY) {       
+    foreach ($ARRAY as $key => $val) {      
         if ($val['id'] == $ID) {          
-            return $key;                   // als hij dus iets vind wat gelijk is aan ['id'] in de array 
+            return $key;                  
         }
     }
-    return null;                           // geeft null terug als antwoord als niks overeenkomt 
+    return null;                   
  }
 
- function printShopCart($ARRAY){                        // de input hiervan is de shoppingcart ????
+ function printShopCart($ARRAY){
     for ($i=0; $i < count($ARRAY); $i++) { 
     echo "Product index: " . $i;
     echo " | Product id: " . $ARRAY[$i]['id'];
@@ -89,7 +32,7 @@ if(isset($_SESSION['amount_cart'])){
     echo "<br>";
 
 }
-echo printShopCart ($shoppingCart); // hiermee laat ik de hele winkelwagentje zien. 
+echo printShopCart ($shoppingCart); 
 
 
 function removeElementFromArray($ELEMENT_INDEX, $ARRAY){
@@ -103,7 +46,7 @@ $shoppingCart = removeElementFromArray(3, $shoppingCart);
 
 echo "<br>";
 echo "<br>";
-echo printShopCart ($shoppingCart);/// items verwijderd uit shoppingCart
+echo printShopCart ($shoppingCart);
 
 
 
